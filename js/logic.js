7bce6c8566c0d1cee31ef9af6fd15a4337caca42
Matej Smid <narudesu@yahoo.com>
2016-04-23 16:08:35 +0200

    grid A added

--- js/logic.js
+++ js/logic.js
@@ -2,15 +2,22 @@ $(document).ready(function() {
   "use strict";
   var
     grid = $(".grid");
-  buildGrid(grid);
+  buildGridA(grid, 5, 4);
 });
 
 
-function buildGrid(grid) {
+function buildGridA(grid, x, y) {
   "use strict";
-  for (var i = 0; i < 16 * 16; i += 1) {
-    var newElement = $("<div class='square'></div>");
+  for (var i = 0; i < x * y; i += 1) {
+    // fill the grid with squares
+    var newElement = $("<div></div>");
+    newElement.addClass('square');
     newElement.appendTo(grid);
-    console.log("added square");
   }
+  var gridWidth = $(".grid").outerWidth();
+  var squareWidth = gridWidth / x;
+  $(".square")
+    .outerWidth(squareWidth)
+    .outerHeight(squareWidth);
+  console.log(gridWidth);
 }
