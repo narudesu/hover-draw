43dd017932c6513b53e9cfc6e7a031a0232e58e3
Matej Smid <narudesu@yahoo.com>
2016-04-23 15:52:46 +0200

    ...

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,16 @@
-/*global $*/
-$(document).ready(function () {
+$(document).ready(function() {
   "use strict";
-  // Have fun.
+  var
+    grid = $(".grid");
+  buildGrid(grid);
 });
+
+
+function buildGrid(grid) {
+  "use strict";
+  for (var i = 0; i < 16 * 16; i += 1) {
+    var newElement = $("<div class='square'></div>");
+    newElement.appendTo(grid);
+    console.log("added square");
+  }
+}
